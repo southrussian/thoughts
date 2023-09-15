5ef3b750925bd726acd3abf9466880582bc88144
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-09-15 23:43:28 +0300

    Declared methods in StorageManager

--- Thoughts/Managers/StorageManager.swift
+++ Thoughts/Managers/StorageManager.swift
@@ -13,4 +13,16 @@ final class StorageManager {
     
     private let storage = Storage.storage()
     private init() { }
+    
+    public func insertBlogPost(with post: String, user: String, completion: @escaping (Bool) -> Void) {
+        
+    }
+    
+    public func getPostsForUser(with post: String, user: String, completion: @escaping ([String]) -> Void) {
+        
+    }
+    
+    public func insertUser(user: String, completion: @escaping (Bool) -> Void) {
+        
+    }
 }
